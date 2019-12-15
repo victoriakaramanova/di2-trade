6e0c8f6bbde228f9df710a525b2b13e14b72d74c
victoriakaramanova <37152322+victoriakaramanova@users.noreply.github.com>
2019-12-15 15:51:37 +0200

    navigation to index from the logo added

--- gulpfile.js
+++ gulpfile.js
@@ -59,5 +59,5 @@ gulp.task("watch", function() {
 gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
 
 gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "opacity of about page changed" && git pull && git push', { stdio: "inherit", shell: true });
+	return cp.spawn('git status && git commit -am "navigation to index from the logo added" && git pull && git push', { stdio: "inherit", shell: true });
 }));
